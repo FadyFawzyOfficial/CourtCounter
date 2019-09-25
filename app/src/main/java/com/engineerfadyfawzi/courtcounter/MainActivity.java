3c2b8b7909adb2d7c0ee45e61686d8e2de885a5c
Fady Fawzi <Engineer.FadyFawzi@gmail.com>
2019-09-25 21:15:07 +0200

    [java, xml] add method displayForTeamA method to displays the given score for Team A, and modify the id of the score TextView: team_a_score

--- app/src/main/java/com/engineerfadyfawzi/courtcounter/MainActivity.java
+++ app/src/main/java/com/engineerfadyfawzi/courtcounter/MainActivity.java
@@ -3,6 +3,7 @@ package com.engineerfadyfawzi.courtcounter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,5 +13,14 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+    }
+    
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA( int score )
+    {
+        TextView scoreView = ( TextView ) findViewById( R.id.team_a_score );
+        scoreView.setText( String.valueOf( score ) );
     }
 }
